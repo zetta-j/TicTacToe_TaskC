015c6e5d7889fc9d3aaeb4411f16dc32ecdd4582
zetta-j <zetta-j@hotmail.com>
2022-01-16 05:47:02 +1300

    Design updates, Class Cell : X player colour changed to green and O player colour changed to yellow.

--- BIT504_A2_TASKC_TICTACTOE2/src/Cell.java
+++ BIT504_A2_TASKC_TICTACTOE2/src/Cell.java
@@ -32,13 +32,13 @@ public class Cell {
 		int x1 = col * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
 		int y1 = row * GameMain.CELL_SIZE + GameMain.CELL_PADDING;
 		if (content == Player.Cross) {
-			graphic2D.setColor(Color.RED);
+			graphic2D.setColor(Color.green);
 			int x2 = (col + 1) * GameMain.CELL_SIZE - GameMain.CELL_PADDING;
 			int y2 = (row + 1) * GameMain.CELL_SIZE - GameMain.CELL_PADDING;
 			graphic2D.drawLine(x1, y1, x2, y2);
 			graphic2D.drawLine(x2, y1, x1, y2);
 		}else if (content == Player.Nought) {
-			graphic2D.setColor(Color.BLUE);
+			graphic2D.setColor(Color.yellow);
 			graphic2D.drawOval(x1, y1, GameMain.SYMBOL_SIZE, GameMain.SYMBOL_SIZE);
 		}
 	}
